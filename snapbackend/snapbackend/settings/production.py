a4aab40c385229d55d4b11ca15b00ccdadcff71f
Rahmi Pruitt <rjpruitt16@gmail.com>
2018-07-23 11:07:46 -0500

    Adding permissions default

--- snapbackend/snapbackend/settings/production.py
+++ snapbackend/snapbackend/settings/production.py
@@ -1,8 +1,14 @@
 from .base import *
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["rjpruitt22.pythonanywhere.com"]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    )
+}
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "images")
 MEDIA_URL = '/snapcapsule/images/'
