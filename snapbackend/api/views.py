76d7b627030ed865c7f656d6f95a72ccc43e7294
Rahmi Pruitt <rjpruitt16@gmail.com>
2018-07-29 04:27:27 +0000

    Working version of site

--- snapbackend/api/views.py
+++ snapbackend/api/views.py
@@ -2,14 +2,24 @@ from django.contrib.auth.models import User
 from api.models import SnapCapsule
 from rest_framework import viewsets
 from api.serializers import UserSerializer, SnapCapsuleSerializer
-from rest_framework.parsers import MultiPartParser, FormParser
 from datetime import datetime, timedelta
+from rest_framework.permissions import IsAuthenticated, AllowAny, IsAdminUser
+from logging import info
+
 class UserViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows users to be viewed or edited.
     """
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    def get_permissions(self):
+        info("User Action: ", self.action)
+        if self.action in ['destroy', "list"]:
+            permission_classes = [IsAdminUser]
+        else:
+            permission_classes = [AllowAny]
+        return [permission() for permission in permission_classes]
 
 class SnapCapsuleViewSet(viewsets.ModelViewSet):
     """
@@ -19,5 +29,18 @@ class SnapCapsuleViewSet(viewsets.ModelViewSet):
     serializer_class = SnapCapsuleSerializer
 
     def get_queryset(self):
-      threshold = datetime.now() + timedelta(days=1)
-      return self.queryset.filter(dateToDelete__range=[datetime.now(), threshold])
+        threshold = datetime.now() + timedelta(days=1)
+        return self.queryset.filter(dateToDelete__range=[datetime.now(), threshold])
+
+    def get_permissions(self):
+        """
+        Instantiates and returns the list of permissions that this view requires.
+        """
+        info("Capsule Action: ", self.action)
+        if self.action == 'destroy':
+            permission_classes = [IsAdminUser]
+        elif self.action == "create":
+            permission_classes = [IsAuthenticated]
+        else:
+            permission_classes = [AllowAny]
+        return [permission() for permission in permission_classes]
--- snapbackend/snapbackend/settings/production.py
+++ snapbackend/snapbackend/settings/production.py
@@ -4,11 +4,7 @@ DEBUG = False
 
 ALLOWED_HOSTS = ["rjpruitt22.pythonanywhere.com"]
 
-REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
-    )
-}
+
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "images")
 MEDIA_URL = '/snapcapsule/images/'
